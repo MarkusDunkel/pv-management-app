cc45b4db781ea112cee3c2c741ef8ac14d395c84
Markus Dunkel <mrks.dunkel@gmail.com>
2025-12-11 11:34:46 +0100

    be: some restructuring of demo package

---
+++ backend/src/main/java/com/pvmanagement/demo/DemoRedemptionRepository.java
@@ -0,0 +1,7 @@
+package com.pvmanagement.repository;
+
+import com.pvmanagement.domain.DemoRedemption;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DemoRedemptionRepository extends JpaRepository<DemoRedemption, Long> {
+}
